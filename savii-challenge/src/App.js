08fa2d9a799b235b14fbc491947e1dedc843affc
Rohail Rafique <rohailrafique@gmail.com>
2022-09-05 14:28:56 -0400

    intial-routes-setup-complete

---
+++ savii-challenge/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+        </Routes>
+      </>
+    </div>
+  );
+}
+
+export default App;
